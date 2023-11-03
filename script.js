501bbda9b0f3c2ed6403cc5380a3d57499e9d845
unknown <CystalJoy.Lomio@gmail.com>
2023-11-03 20:39:49 +0800

    Done Web development

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log(thisIsSum(22, 24))
+// console.log(thisIsDiff(30, 50))
+// function thisIsSum(myNumber, second){
+//     return myNumber + second
+// }
+// function thisIsDiff(myNumber,second)
+// {
+//     return myNumber - second
+// }
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
